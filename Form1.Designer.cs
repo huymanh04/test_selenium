1b0e02bc5d040c672539cb1b4b148c6f8782c129
manhc <manhc@LAPTOP-RG1FFODJ>
2025-04-18 17:19:54 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace test_selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(63, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(448, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(463, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(320, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(182, 34);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(182, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(320, 88);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 20);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(557, 39);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace test_selenium
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("ook.txt", "mạnh"+Environment.NewLine);
+            var maaa = File.ReadAllLines("");
+            for (int i = 0; i < 1; i++)
+            {
+
+
+                Thread t = new Thread(() =>
+                {
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        label1.Text ="bắt đầu rồi";
+                    });
+                    //        IWebDriver chrome = null; List<int> driverProcessIds = new List<int>();
+
+                    //        IJavaScriptExecutor js = null;
+
+                    //            ChromeOptions chromeOptions = new ChromeOptions();
+                    //            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+                    //            chromeDriverService.HideCommandPromptWindow = true;
+                    //            chromeOptions.AddArguments("start-maximized");
+                    //            chromeOptions.AddArgument("--disabel-notifications");
+                    //            chromeOptions.AddExcludedArgument("enable-automation");
+                    //            chromeOptions.AddArguments(new string[]
+                    //            {
+                    //"--disable-3d-apis",
+                    //        "--app=https://www.tiktok.com/login/phone/forget-password",
+                    //"--disable-background-networking",
+                    //"--disable-bundled-ppapi-flash",
+                    //"--disable-client-side-phishing-detection",
+                    //"--disable-default-apps",
+                    //"--disable-hang-monitor",
+                    //"--disable-gpu",
+                    //"--no-sandbox",
+                    //"--disable-prompt-on-repost",
+                    //"--disable-sync",
+                    //"--use-fake-device-for-media-stream",
+                    //"--use-fake-ui-for-media-stream",
+                    //"--disable-webgl",
+                    //"--enable-blink-features=ShadowDOMV0",
+                    //"--enable-logging",
+                    //"--disable-notifications",
+                    //"--disable-dev-shm-usage",
+                    //"--disable-web-security",
+                    //"--lang=vn",
+                    //"--disable-rtc-smoothness-algorithm",
+                    //"--disable-webrtc-hw-decoding",
+                    //"--disable-webrtc-hw-encoding",
+                    //"--disable-webrtc-multiple-routes",
+                    //"--disabel-images",
+                    //"--disable-webrtc-hw-vp8-encoding",
+                    //"--enforce-webrtc-ip-permission-check",
+                    //"--force-webrtc-ip-handling-policy",
+                    //"--ignore-certificate-errors",
+                    //"--disable-infobars",
+                    //"--disable-popup-blocking",
+                    //"--enable-precise-memory-info",
+                    //"--disable-3d-apis",
+                    //"--start-maximized",
+
+                    //"--disable-blink-features=\"BlockCredentialedSubresources\"",
+                    //"--mute-audio",
+
+                    //"--window-size=450,600",
+                    //"--disable-popup-blocking" });
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.geolocation", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.notifications", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.plugins", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.popups", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.auto_select_certificate", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.mixed_script", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.media_stream", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.media_stream_mic", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.media_stream_camera", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.protocol_handlers", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.midi_sysex", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.push_messaging", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.ssl_cert_decisions", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.metro_switch_to_desktop", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.protected_media_identifier", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.site_engagement", 1);
+                    //            chromeOptions.AddUserProfilePreference("profile.default_contextn[ii,0]t_setting_values.durable_storage", 1);
+                    //            chromeOptions.AddArgument("--disable-blink-features=AutomationControlled");
+                    //            chromeOptions.AddUserProfilePreference("credentials_enable_service", false);
+                    //            chromeOptions.AddArgument("--disabel-notifications");
+                    //            chromeOptions.AddArgument("--disabel-images");
+                    //            //chromeOptions.AddArgument("--proxy-server=119.3.113.150:9094");/// fake proxy dạng http https 119.3.113.150:9094 proxy https
+
+                    //        chrome = new ChromeDriver(chromeDriverService, chromeOptions);
+                    //        chrome.Navigate().GoToUrl("https://www.tinywebgallery.com/blog/advanced-iframe/free-iframe-checker");
+                    //        Thread.Sleep(1000);
+                    //        try { 
+                    //        chrome.SwitchTo().Frame(chrome.FindElement(By.Name("advanced_iframe")));
+                    //            chrome.SwitchTo().DefaultContent();
+                    //    var m=    chrome.FindElement(By.Name("submit")).GetAttribute("value");
+                    //            chrome.SwitchTo().DefaultContent();
+                    //            chrome.FindElement(By.XPath("/html/body/div/div/div/main/article/div/div/p[3]"));
+                    //        }
+                    //        catch { }
+                    HttpRequest h = new HttpRequest();
+                    //var m = h.Get("https://api.viotp.com/users/balance?token=e208f71988894ea1ad4296f602db9dd7").ToString();
+                    //string pattern = "\"balance\"\\s*:\\s*(\\d+)";
+
+                    //Match match = Regex.Match(m, pattern);
+                    //if (match.Success)
+                    //{
+                    //    int balance = int.Parse(match.Groups[1].Value);
+                    //    Console.WriteLine("Balance: " + balance);
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("Không tìm thấy balance.");
+                    //}
+                    //string data = "{\"api_key\": \"f22233ec016d5207cd7af94f8a6a168e\"}";
+                    //var ma = h.Post("https://api.otpmmo.xyz/api/customer/user-info", data, "application/json").ToString();
+                    //JObject obj = JObject.Parse(ma);
+                    //string amount = (string)obj["data"]["amount"];
+                    var m = h.Get("https://api.viotp.com/request/getv2?token=e208f71988894ea1ad4296f602db9dd7&serviceId=698").ToString();
+                    Thread.Sleep(1000);
+                    JObject obj = JObject.Parse(m);
+                    string rePhone = (string)obj["data"]["re_phone_number"];
+                    long requestId = (long)obj["data"]["request_id"];
+                    string ma = "";
+                    for (int ia = 0; ia < 60; ia++)
+                    {
+
+
+                        var mn = h.Get($"https://api.viotp.com/session/getv2?requestId={requestId}&token=e208f71988894ea1ad4296f602db9dd7").ToString();
+                  
+                        JObject obaj = JObject.Parse(mn);
+                        JToken smsContentToken = obaj["data"]["SmsContent"];
+
+                        string smsContent = smsContentToken?.ToString() ?? "null";
+                        if (smsContent != null)
+                        {
+                            JToken smsContentTokena= obaj["data"]["Code"];
+                            ma = smsContentTokena.ToString();
+                            break;
+                        }
+                        Thread.Sleep(1000);
+                    }
+                    File.WriteAllText("ook.txt", "mạnh");
+                }); t.Start();
+        }
+        }
+    }
+}
